ae7bfa235aef98d6a1308b28e25322943b105342
Nily <nghulam-@student.42.fr>
2023-12-10 15:41:53 +0100

    deeper error checks, fixed bugs, organizing and normalizing map_generator

--- map_generator/main.c
+++ map_generator/main.c
@@ -2,67 +2,79 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <time.h>
-#include "libft/include/vanilla.h"
+#include "libft.h"
 
-void    write_file(char *str, int fd)
+void	random_fill(int fd, int player)
 {
-    write(fd, str, ft_strlen(str));
+	static int	count = 0;
+	int			random;
+
+	if (count++ == player)
+	{
+		random = rand() % 4;
+		if (random == 0)
+			write(fd, "N", 1);
+		else if (random == 1)
+			write(fd, "S", 1);
+		else if (random == 2)
+			write(fd, "E", 1);
+		else
+			write(fd, "W", 1);
+		return ;
+	}
+	if (rand() % 5 == 0)
+		write(fd, "1", 1);
+	else
+		write(fd, "0", 1);
 }
 
-void        create_map(int height, int width, int fd)
+void	create_map(int height, int width, int fd)
 {
-    int	i;
+	int	i;
 	int	j;
+	int	player;
 
-    i = 0;
+	player = rand() % (height * width - 2 * (height + width - 2));
+	i = 0;
 	while (i < height)
 	{
 		j = 0;
 		while (j < width && (i == 0 || i == height - 1))
-        {
-            write_file("1", fd);
+			j += (write(fd, "1", 1), 1);
+		if (j == 0)
+			j += (write(fd, "1", 1), 1);
+		while (j < width - 1)
+		{
+			random_fill(fd, player);
 			j++;
-        }
-        if (j == 0)
-        {
-            write_file("1", fd);
-            j++;
-        }     
-        while (j < width - 1)
-        {
-            if (rand() % 5 == 0)
-                write_file("1", fd);
-            else
-                write_file("0", fd);
-            j++;
-        }
-        if (j == width - 1)
-            write_file("1", fd);
-        write_file("\n", fd);
+		}
+		if (j == width - 1)
+			write(fd, "1", 1);
+		write(fd, "\n", 1);
 		i++;
 	}
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    // ./executable height width
+	int	fd;
 
-    int fd;
-
-    fd = open("../map_generated.cub", O_CREAT | O_RDWR);
-
-    if (argc > 3 || !argv[1] || !argv[2] || read(fd, "", 0) == -1)
-        return (0);
-
-    write_file("NO ./pics/bluestone.xpm\n\n", fd);
-    write_file("SO ./pics/colorstone.xpm\n\n", fd);
-    write_file("WE ./pics/greystone.xpm\n\n", fd);
-    write_file("EA ./pics/redbrick.xpm\n\n", fd);
-    write_file("F 220, 100, 0\n\n", fd);
-    write_file("C 225, 30, 0\n\n", fd);
-
-    srand(time(NULL));
-    
-    create_map(ft_atoi(argv[1]), ft_atoi(argv[2]), fd);
-
+	if (argc > 3 || !argv[1] || !argv[2] || \
+		ft_atoi(argv[1]) < 3 || ft_atoi(argv[2]) < 3)
+	{
+		write(2, "Error args ; need height and width (integers, >= 3).\n", 53);
+		return (EXIT_FAILURE);
+	}
+	fd = open("../map_generated.cub", O_CREAT | O_RDWR | O_TRUNC);
+	if (fd < 0 || read(fd, NULL, 0) < 0)
+		return (write(2, "Error file.\n", 12), EXIT_FAILURE);
+	write(fd, "NO ./pics/bluestone.xpm\n\n", 25);
+	write(fd, "SO ./pics/colorstone.xpm\n\n", 26);
+	write(fd, "WE ./pics/greystone.xpm\n\n", 25);
+	write(fd, "EA ./pics/redbrick.xpm\n\n", 24);
+	write(fd, "F 220,100,0\n\n", 13);
+	write(fd, "C 225,30,0\n\n", 12);
+	srand(time(NULL));
+	create_map(ft_atoi(argv[1]), ft_atoi(argv[2]), fd);
+	return (EXIT_SUCCESS);
 }
